c4f3be3970ca578a996bd9ae87ffd80dbbfb6ca3
Emma Gerigscott <elgerig@gmail.com>
2023-07-06 17:12:36 -0700

    add guesses left to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import lettersGuessedReducer from './guess-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
 import gameEndReducer from './game-end-reducer';
+import guessesLeftReducer from './guesses-left-reducer';
 
 const rootReducer = combineReducers({
     formVisibleOnPage: formVisibleReducer,
     lettersGuessed: lettersGuessedReducer,
-    gameEnd: gameEndReducer
+    gameEnd: gameEndReducer,
+    guessesLeft: guessesLeftReducer
 });
 
 export default rootReducer;
